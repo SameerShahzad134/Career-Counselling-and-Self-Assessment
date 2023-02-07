7b92758b04829ee53693ea4c5d439cbea7c64a6e
Sameer Shahzad <bcsf19a030@pucit.edu.pk>
2023-02-08 03:27:53 +0500

    still pending

--- CCASA/obj/Debug/netcoreapp3.1/CCASA.AssemblyInfo.cs
+++ CCASA/obj/Debug/netcoreapp3.1/CCASA.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("296e1135-c766-4848-9b04-05e7cce94621")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("CCASA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
